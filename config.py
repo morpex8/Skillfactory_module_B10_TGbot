9b34db1b329ef0e49e5d63330b1597396129dadf
Soulfox <97559525+morpex8@users.noreply.github.com>
2023-02-02 02:26:41 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN = "6025972205:AAHm3voDF5oW027fAhRgjuKUUUIz-IYPcHM"
+API_KEY = 'WrhIbEPUz0Cj837vbaxjTQodFabC50FR' #apilayer
+keys = {'Доллар': 'USD',
+        'Евро': 'EUR',
+        'Рубль': 'RUB',
+        'Юань': 'CNY',
+        'Белорусский рубль': 'BYN',}
